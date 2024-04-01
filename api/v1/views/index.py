7109ed8d6b7cc80da7d1d9ac180e0a2a4b689d6b
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-04-01 05:19:27 +0000

    status API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""Define routes for blueprint
+"""
+
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from models import storage
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
-
 @app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
+def status():
+    """Return status of application
+    """
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
+def counts():
+    """Retrieve counts of various objects in storage."""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
 
-if __name__ == "__main__":
-    pass
+    class_map = {"amenity_counts": Amenity, "city_counts": City,
+                 "place_counts": Place, "review_counts": Review,
+                 "state_counts": State, "user_counts": User}
+    counts_dict = {}
+
+    for key, model in class_map.items():
+        counts_dict[key] = storage.count(model)
+
+    return jsonify(counts_dict)
